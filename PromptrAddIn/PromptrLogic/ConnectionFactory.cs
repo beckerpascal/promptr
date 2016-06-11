305f9a8633a804a94d0221c051790ff5b53977fc
kbuersti <kevin.steinbach@gmx.de>
2016-06-11 18:29:13 +0200

    First API Connection

---
+++ PromptrAddIn/PromptrLogic/ConnectionFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using PromptrLib;
+using Q42.HueApi.NET;
+using Q42.HueApi;
+using Q42.HueApi.Interfaces;
+
+namespace PromptrLib
+{
+    public class ConnectionFactory
+    {
+        private readonly string appKey;
+
+        public ConnectionFactory(string appKey)
+        {
+            this.appKey = appKey;
+        }
+
+        public Connection GetConnection()
+        {
+            ILocalHueClient client = new LocalHueClient("10.0.1.2");
+            client.Initialize(appKey);
+
+            return new Connection(client);
+        }
+
+        
+    }
+}
