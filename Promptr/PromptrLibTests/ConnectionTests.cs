6ed7cf826287a067d06dab8cebd44a9a817255aa
kbuersti <kevin.steinbach@gmx.de>
2016-06-12 04:24:17 +0200

    ConnectionTests fix

--- Promptr/PromptrLibTests/ConnectionTests.cs
+++ Promptr/PromptrLibTests/ConnectionTests.cs
@@ -3,7 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PromptrLib;
-using Q42.HueApi;
+using PromptrLib.Connector;
 
 namespace PromptrLibTests
 {
